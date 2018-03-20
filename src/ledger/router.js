b6063497310888a16da42d9b382443a0f99fccb4
Andy Chen <amazingandyyy@gmail.com>
2018-03-20 16:20:03 -0700

    can register node

--- src/ledger/router.js
+++ src/ledger/router.js
@@ -1,11 +1,13 @@
 import Transaction from './transaction';
 import Blockchain from './blockchain';
 import genesisBlock from './genesisBlock';
+import BlockchainNode from './blockchainNode';
 
 const router = require('express').Router();
 
 let transactions = [];
 let blockchain = new Blockchain(genesisBlock);
+let nodes = [];
 
 router.get('/', (req, res)=>{
   res.json(blockchain);
@@ -22,6 +24,20 @@ router.post('/transactions', (req, res)=>{
   let transaction = new Transaction(from , to ,amount)
   transactions.push(transaction);
   res.json(transactions);
+})
+
+router.post('/nodes/register', (req, res)=>{
+  let nodeList = req.body.urls;
+  if(!nodeList) return res.sendStatus(500);
+  nodeList.forEach((n)=>{
+    let node = new BlockchainNode(n.url);
+    nodes.push(node);
+  })
+  res.json(nodes);
+})
+
+router.get('/nodes', (req, res)=>{
+  res.json(nodes);
 })
 
 export default router;
